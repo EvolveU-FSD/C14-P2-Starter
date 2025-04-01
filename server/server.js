3a7a605fa1dc3fd4d133f378a5dbeb34e37ce46d
shaneinceptionu <shane@inceptionu.com>
2025-04-01 13:47:01 -0600

    added Cors and super hero registry

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-
+import cors from 'cors';
 import superHeroRoutes from './routes/superHeroRoutes.js';
 import cityRoutes from './routes/cityRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/superheroes', superHeroRoutes);
